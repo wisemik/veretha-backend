3f45c03b42db43aca246d7e520fb9facef3ddb34
Mik Wiseman <hi@wisemik.com>
2024-09-22 02:20:08 +0800

    Worldcoin fixes

--- hrapi.py
+++ hrapi.py
@@ -10,17 +10,17 @@ from fastapi import FastAPI
 import os
 from pydantic import BaseModel, EmailStr
 import requests
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, Boolean, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
 from passlib.context import CryptContext
 import circle_veretha
+
 # Load environment variables from .env file
 load_dotenv()
 
 PROXYCURL_API_KEY = os.getenv('PROXYCURL_API_KEY')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 openai.api_key = OPENAI_API_KEY
-
 
 # FastAPI app instance
 app = FastAPI()
@@ -40,7 +40,6 @@ DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
 # User table for registration
 class User(Base):
     __tablename__ = 'users'
@@ -54,7 +53,7 @@ class User(Base):
     country = Column(String)
     city = Column(String)
     linkedin_url = Column(String)
-    verified = Column(String, default="not")  # Can be 'not', 'device', or 'orb'
+    verified = Column(Boolean, default=False)  # Changed to Boolean
     wallet_id = Column(String)
     wallet_address = Column(String)
 
@@ -72,12 +71,10 @@ def get_db():
     finally:
         db.close()
 
-
 def generate_wallet_id_and_address(email: str):
     """Generate wallet_id and wallet_address using the email as a base."""
     wallet_id, wallet_address = circle_veretha.create_wallet(email, email, email)
     return wallet_id, wallet_address
-
 
 # Pydantic model for user registration
 class UserCreate(BaseModel):
@@ -90,8 +87,7 @@ class UserCreate(BaseModel):
     country: str = ""
     city: str = ""
     linkedin_url: str = ""
-    verified: str = "not"
-
+    verified: bool = False  # Changed to boolean
 
 # Register a new user
 @app.post("/register")
@@ -117,13 +113,12 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
         country=user.country,
         city=user.city,
         linkedin_url=user.linkedin_url,
-        verified=user.verified,
+        verified=user.verified,  # Accept boolean
         wallet_id=wallet_id,
         wallet_address=wallet_address
     )
     db.add(new_user)
     db.commit()
-
 
     print(f"New user registered: {new_user.wallet_address}, {new_user.wallet_id}")
     return {
@@ -136,7 +131,7 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
         "country": new_user.country,
         "city": new_user.city,
         "linkedin_url": new_user.linkedin_url,
-        "verified": new_user.verified,
+        "verified": new_user.verified,  # Return boolean
         "wallet_id": new_user.wallet_id,
         "wallet_address": new_user.wallet_address
     }
@@ -161,10 +156,11 @@ def get_profile(email: str, db: Session = Depends(get_db)):
         "country": db_user.country,
         "city": db_user.city,
         "linkedin_url": db_user.linkedin_url,
-        "verified": db_user.verified,
+        "verified": db_user.verified,  # Return boolean
         "wallet_id": db_user.wallet_id,
         "wallet_address": db_user.wallet_address
     }
+
 
 class VerifyRequest(BaseModel):
     nullifier_hash: str
@@ -172,7 +168,6 @@ class VerifyRequest(BaseModel):
     proof: str
     verification_level: str
     action: str
-
 
 @app.post("/verify")
 async def verify(req_body: VerifyRequest):
@@ -205,65 +200,34 @@ async def verify(req_body: VerifyRequest):
         print(f"Error occurred: {e}")
         raise HTTPException(status_code=500, detail="Error communicating with World ID verification service.")
 
-# Set verification status
-# POST method to set the verification status
 
-class SetVerifiedModel(BaseModel):
+# Model to handle verification status update
+class EmailModel(BaseModel):
     email: EmailStr
-    verification_type: str  # 'device' or 'orb'
-
 
 @app.post("/set-verified")
-def set_verified(data: SetVerifiedModel, db: Session = Depends(get_db)):
+def set_verified(email_model: EmailModel, db: Session = Depends(get_db)):
     # Find the user by email
-    db_user = db.query(User).filter(User.email == data.email).first()
-
-    # If user is not found, raise an error
-    if not db_user:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    # Update the user's verification status
-    if data.verification_type not in ['device', 'orb']:
-        raise HTTPException(status_code=400, detail="Invalid verification type")
-
-    db_user.verified = data.verification_type
-    db.commit()
-    db.refresh(db_user)
-
-    return {"message": f"User {db_user.email} verification status updated to {db_user.verified}"}
-
-
-
-
-# Login method (for completeness)
-class UserAuth(BaseModel):
-    email: EmailStr
-    password: str
-
-
-@app.get("/get-profile/{email}")
-def get_profile(email: str, db: Session = Depends(get_db)):
-    # Find the user by email
+    email = email_model.email
+    print(f"Setting verification status for user {email} to True")
     db_user = db.query(User).filter(User.email == email).first()
 
     # If user is not found, raise an error
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    # Return the user's profile details
-    return {
-        "id": db_user.id,
-        "email": db_user.email,
-        "full_name": db_user.full_name,
-        "occupation": db_user.occupation,
-        "company": db_user.company,
-        "skills": db_user.skills,
-        "country": db_user.country,
-        "city": db_user.city,
-        "linkedin_url": db_user.linkedin_url,
-        "wallet_address": db_user.wallet_address
-    }
+    # Update the user's verification status to True
+    db_user.verified = True  # Always set to True
+    db.commit()
+    db.refresh(db_user)
 
+    return {"message": f"User {db_user.email} verification status updated to {db_user.verified}"}
+
+
+# Login method
+class UserAuth(BaseModel):
+    email: EmailStr
+    password: str
 
 @app.post("/login")
 def login_user(user: UserAuth, db: Session = Depends(get_db)):
@@ -284,21 +248,12 @@ def login_user(user: UserAuth, db: Session = Depends(get_db)):
         "country": db_user.country,
         "city": db_user.city,
         "linkedin_url": db_user.linkedin_url,
-        "verified": db_user.verified,
+        "verified": db_user.verified,  # Return boolean
         "wallet_id": db_user.wallet_id,
         "wallet_address": db_user.wallet_address
     }
 
-
-class ScoreRequest(BaseModel):
-    resume_text: str
-    job_description: str
-
-
-class LinkedInRequest(BaseModel):
-    linkedin_url: str
-
-
+# File upload and text extraction
 @app.post("/extract-text")
 async def extract_text_from_pdf(file: UploadFile = File(...)):
     if file.content_type == 'application/pdf':
@@ -314,94 +269,6 @@ async def extract_text_from_pdf(file: UploadFile = File(...)):
     else:
         return {"error": "Invalid file type. Please upload a PDF."}
 
-
-def fetch_linkedin_profile(linkedin_url):
-    api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
-    headers = {'Authorization': 'Bearer ' + PROXYCURL_API_KEY}
-    params = {
-        'linkedin_profile_url': linkedin_url,
-        'use_cache': 'if-present',  # Use cache to reduce API cost
-        'fallback_to_cache': 'on-error',  # Fallback to cache if there's an error
-    }
-
-    response = requests.get(api_endpoint, params=params, headers=headers)
-
-    if response.status_code == 200:
-        print(f"LinkedIn profile fetched successfully for URL: {linkedin_url}")
-        return response.json()
-    else:
-        print(f"Failed to fetch LinkedIn profile: {response.status_code} - {response.text}")
-        return {}
-
-
-@app.post("/score-resume")
-async def score_resume(request: ScoreRequest):
-    print(f"Received resume text: {request.resume_text}")
-    print(f"Received job description: {request.job_description}")
-    messages = generate_prompt_messages(request.resume_text, request.job_description)
-
-    try:
-        response = openai.chat.completions.create(model="gpt-4o-mini",
-                                                  messages=messages)
-        content = response.choices[0].message.content
-        print(f"Received response from OpenAI: {content}")
-        result = json.loads(content)
-
-        score = result.get("score", 0)
-        description = result.get("description", "No description provided.")
-        details = result.get("details", "No details provided.")
-        print(f"Extracted score: {score}, description: {description}, improvements: {details}")
-        return {"score_result": score, "description": description, "improvements": details}
-    except Exception as e:
-        return {"error": str(e)}
-
-
-@app.post("/extract-linkedin")
-async def extract_linkedin(linkedin_request: LinkedInRequest):
-    linkedin_url = linkedin_request.linkedin_url
-    print(f"Received LinkedIn URL: {linkedin_url}")
-    profile_data = fetch_linkedin_profile(linkedin_url)
-    print(f"Profile data: {profile_data}")
-
-    return {"linkedin_data": profile_data}
-
-
-def generate_prompt_messages(resume_text, job_description):
-    prompt = f"""
-                   You are a career consultant helping candidates assess how well their resume matches
-                    a specific job vacancy. 
-                   You will be provided with the candidate's resume and the job description. 
-                   Your task is to provide an objective evaluation of how well the resume fits the job requirements, 
-                   give feedback, and offer suggestions on how the resume can be improved for a better match.
-                   Be critical, find the way to improve CV.
-
-                   Please provide the result in JSON format (only JSON!! without any additional symbols), containing the
-                    following fields:
-                   - "score": a string  (0-100) representing the match between the resume and the job description, where
-                    0 - no match at all and 100 means a absolutely perfect match.
-                   - "description": a string containing feedback on the score: why that score, and how well the resume
-                    fits the job requirements.
-                   - "details": a string containing html with suggestions for improving the resume to better match the
-                    job description in form of multiple suggestions. 
-
-
-                   Candidate's resume: {resume_text}
-                   Job description: {job_description}
-
-                   The output must be in JSON!! without any additional symbols. 
-                   Only the dictionary itself.
-               """
-    messages = [
-        {
-            "role": "system",
-            "content": "You are a career consultant helping candidates improve their resumes. "
-                       "Always respond in JSON format. Output should be JSON only!!"
-                       "NO additional symbols. Only the dictionary itself."
-        },
-        {"role": "user", "content": prompt}
-    ]
-
-    return messages
-
+# Start the app
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
